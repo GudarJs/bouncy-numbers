3807924b360e94c8cbf0430a573bbe3d1dd6c3f5
Guzmán D. Darío <guzman.d.dario@gmail.com>
2019-02-06 00:13:37 -0500

    Improve test documentation

--- tests/bouncy/utils_test.py
+++ tests/bouncy/utils_test.py
@@ -1,7 +1,8 @@
 from bouncy.utils import is_bouncy, calculate_least_number
 
 """
-Values were taken from the problem statement
+Test if the is_bouncy function is returning True if a number is bouncy or False otherwise.
+Values were taken from the problem statement.
 """
 def test_is_bouncy():
     assert is_bouncy(134468) == False
@@ -9,6 +10,7 @@ def test_is_bouncy():
     assert is_bouncy(155349) == True
 
 """
+Test if the calculate_least_number function is returning the least number for a given proportion of bouncy numbers.
 Values were taken from the problem statement
 """
 def test_calculate_least_number():
